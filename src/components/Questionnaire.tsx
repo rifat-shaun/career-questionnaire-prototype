78cb73ad183fc84a62440538010fdd220934f808
rifat-hasan <mdrifathasanshaun@gmail.com>
2024-09-24 23:18:30 +0600

    feat: added pair design

--- src/components/Questionnaire.tsx
+++ src/components/Questionnaire.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import {
   Radio,
@@ -6,39 +6,91 @@ import {
   FormControlLabel,
   FormControl,
   FormLabel,
+  Card,
+  Typography,
+  Button,
 } from "@mui/material";
 import { DUMMY_QUESTIONS } from "../constants/DummyQuestions";
 
+type Language = "en" | "et" | "ru";
+
 const Questionnaire: React.FC = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState<Language>("en");
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: string]: string;
+  }>({});
 
-  const options = (translationKey: string) => {
-    const optionsArray = t(translationKey, {
-      returnObjects: true,
-    }) as string[];
-
-    return Array.isArray(optionsArray) ? optionsArray : [];
+  const handleSelect = (id: string, choice: "a" | "b") => {
+    setSelectedOptions((prev) => ({ ...prev, [id]: choice }));
   };
+
+  useEffect(() => {
+    setSelectedLanguage((i18n?.language as Language) || "en");
+  }, [i18n.language]);
 
   return (
     <div className="p-4 max-w-md mx-auto">
       <FormControl component="fieldset">
         {DUMMY_QUESTIONS.map((item) => (
           <div key={item.id}>
-            <FormLabel component="legend">{t(item.en.question)}</FormLabel>
-            <RadioGroup name="education">
-              <>{console.log(options(`${item.id}_options`), 123)}</>
-              {options(`${item.id}_options`).map(
-                (option: string, index: number) => (
-                  <FormControlLabel
-                    key={index}
-                    value={option}
-                    control={<Radio />}
-                    label={option}
-                  />
-                )
-              )}
-            </RadioGroup>
+            {item[selectedLanguage]?.options ? (
+              <>
+                <FormLabel component="legend">
+                  {t(item[selectedLanguage].question)}
+                </FormLabel>
+                <RadioGroup name="education">
+                  {(item[selectedLanguage]?.options || []).map(
+                    (option: string, index: number) => (
+                      <FormControlLabel
+                        key={index}
+                        value={option}
+                        control={<Radio />}
+                        label={option}
+                      />
+                    )
+                  )}
+                </RadioGroup>
+              </>
+            ) : (
+              <>
+                <FormLabel component="legend">
+                  {t(item[selectedLanguage].question)}
+                </FormLabel>
+                {item[selectedLanguage]?.pairs?.map((pair) => (
+                  <Card
+                    key={pair.id}
+                    className="mb-4 p-4 w-full max-w-md gap-2"
+                  >
+                    <Typography>
+                      {pair.a} vs {pair.b}
+                    </Typography>
+                    <div className="flex justify-around mt-4 gap-2">
+                      <Button
+                        variant={
+                          selectedOptions[pair.id] === "a"
+                            ? "contained"
+                            : "outlined"
+                        }
+                        onClick={() => handleSelect(pair.id, "a")}
+                      >
+                        {pair.a}
+                      </Button>
+                      <Button
+                        variant={
+                          selectedOptions[pair.id] === "b"
+                            ? "contained"
+                            : "outlined"
+                        }
+                        onClick={() => handleSelect(pair.id, "b")}
+                      >
+                        {pair.b}
+                      </Button>
+                    </div>
+                  </Card>
+                ))}
+              </>
+            )}
           </div>
         ))}
       </FormControl>
--- src/i18n.ts
+++ src/i18n.ts
@@ -2,18 +2,11 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import enTranslation from "./locales/en.json";
-import etTranslation from "./locales/et.json";
-import ruTranslation from "./locales/ru.json";
-
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: { translation: enTranslation },
-      et: { translation: etTranslation },
-      ru: { translation: ruTranslation },
     },
     fallbackLng: "en",
     interpolation: {
